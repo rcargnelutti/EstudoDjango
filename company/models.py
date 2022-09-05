be13c91c07ec807de9aa37004b23a208bb70e0c2
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-09-05 17:08:01 -0300

    Class Comapny e Entity

--- company/models.py
+++ company/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    document = models.CharField(max_length=25)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Entity(models.Model):
+    name = models.CharField(max_length=255)
+    document = models.CharField(max_length=25)
+    active = models.BooleanField(default=True)
+    inicio_contrato = models.DateTimeField()
+    fim_contrato = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
