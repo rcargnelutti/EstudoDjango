869d9530546f179e71ea9e53e614708b0be0342b
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-09-06 10:20:49 -0300

    Model  Company

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from company.models import Company, Entity
+
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Entity)
--- company/models.py
+++ company/models.py
@@ -15,12 +15,12 @@ class Entity(models.Model):
     name = models.CharField(max_length=255)
     document = models.CharField(max_length=25)
     active = models.BooleanField(default=True)
-    inicio_contrato = models.DateTimeField()
-    fim_contrato = models.DateTimeField()
+    inicio_contrato = models.DateTimeField(blank=True)
+    fim_contrato = models.DateTimeField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True)
+
     def __str__(self):
         return self.name
-
-
