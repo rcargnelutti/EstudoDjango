759f452ad16b01262e5a4ea371acad1d9c5683e9
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-09-09 14:45:12 -0300

    API Company

---
+++ company/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from company.models import Company
+
+class CompanySerializer(ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
--- company/views.py
+++ company/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from company.models import Company
+from company.serializers import CompanySerializer
+
+class CompanyViewSet(ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    # http_method_names = ['get', 'options', 'head', 'patch', 'post', 'delete']
