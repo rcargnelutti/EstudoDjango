4d4340fd3a31d62ec71e1cc180a9b5da280ba4bd
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-09-09 16:40:42 -0300

    Testando filtros

--- company/views.py
+++ company/views.py
@@ -1,5 +1,6 @@
+from xmlrpc.client import boolean
 from rest_framework.viewsets import ModelViewSet
-
+from django.db.models import Q
 from company.models import Company
 from company.serializers import CompanySerializer
 
@@ -9,6 +10,22 @@ class CompanyViewSet(ModelViewSet):
     # http_method_names = ['get', 'options', 'head', 'patch', 'post', 'delete']
 
     # sobrescrevendo queryset para filtrar/buscar por companies active
-    def get_queryset(self):
-        active = Company.objects.filter(active=True)
-        return active
+    # def get_queryset(self):
+    #     company = Company.objects.filter(active=True, name='AVACON')
+    #     return company
+
+    # def get_queryset(self):
+    #     qs = super().get_queryset()
+    #     active = self.request.query_params.get('active')
+    #     qs = qs.filter(active=active)
+    #     return qs
+
+    # def get_queryset(self):
+    #     qs = Company.objects.all()
+    #     active = self.request.query_params.get('active')
+    #     print(active)
+    #     # if active is not None:
+    #     qs = qs.filter(active=active)
+    #     return qs
+
+    
