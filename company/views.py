e004cd79a46793093cb6eb343a6ea04143b08aff
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-09-09 17:11:45 -0300

    Aplicando filtros com django_filters

--- company/views.py
+++ company/views.py
@@ -1,12 +1,14 @@
 from xmlrpc.client import boolean
 from rest_framework.viewsets import ModelViewSet
-from django.db.models import Q
+from django_filters.rest_framework import DjangoFilterBackend
 from company.models import Company
 from company.serializers import CompanySerializer
 
 class CompanyViewSet(ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'document','active']
     # http_method_names = ['get', 'options', 'head', 'patch', 'post', 'delete']
 
     # sobrescrevendo queryset para filtrar/buscar por companies active
@@ -28,4 +30,3 @@ class CompanyViewSet(ModelViewSet):
     #     qs = qs.filter(active=active)
     #     return qs
 
-    
