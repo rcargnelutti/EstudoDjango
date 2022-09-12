cf57dc7f69c4c688dd8c2f771bc2a1ff4fc89e92
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-09-12 17:09:49 -0300

    Reorganização das URLs/Routers

--- estudodjango/urls.py
+++ estudodjango/urls.py
@@ -1,20 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# from rest_framework import routers
+from rest_framework import routers
 
-# from company.views import CompanyViewSet
-# from entity.views import EntityViewSet
+from company.views import CompanyViewSet
+from entity.views import EntityViewSet, UnityViewSet
 
-# router = routers.DefaultRouter()
-# router.register(r'companies', CompanyViewSet)
-# router.register(r'entities', EntityViewSet)
+router = routers.DefaultRouter()
+router.register(r'companies', CompanyViewSet)
+router.register(r'entities', EntityViewSet)
+router.register(r'units', UnityViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include(router.urls)),
-
-    path('api/company/', include('company.urls')),
-    path('api/entity/', include('entity.urls')),
-    
+    path('api/', include(router.urls)),
 ]
