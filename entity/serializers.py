bcd5ef5899adaebb431140b62e6e8d386d7c56cb
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-09-12 17:10:35 -0300

    Endpoint Units

--- entity/serializers.py
+++ entity/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework.serializers import ModelSerializer
-from entity.models import Entity
+from entity.models import Entity, Unity
 
 class EntitySerializer(ModelSerializer):
     class Meta:
         model = Entity
+        fields = '__all__'
+
+
+class UnitySerializer(ModelSerializer):
+    class Meta:
+        model = Unity
         fields = '__all__'
--- entity/views.py
+++ entity/views.py
@@ -1,7 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
-from entity.models import Entity
-from entity.serializers import EntitySerializer
+from entity.models import Entity, Unity
+from entity.serializers import EntitySerializer, UnitySerializer
 
 class EntityViewSet(ModelViewSet):
     queryset = Entity.objects.all()
     serializer_class = EntitySerializer
+
+
+class UnityViewSet(ModelViewSet):
+    queryset = Unity.objects.all()
+    serializer_class = UnitySerializer
