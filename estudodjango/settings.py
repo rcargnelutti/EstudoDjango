86c17768e29ee60496a9bdec6ded6d770b9cac60
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-09-14 17:10:49 -0300

    Configurando tempo de sessaõ do token JWT

--- estudodjango/settings.py
+++ estudodjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -140,4 +141,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ) 
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta (minutes=720),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1)
 }
